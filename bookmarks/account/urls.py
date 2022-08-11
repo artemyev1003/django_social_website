984d22ba4a14f5d3834aeaa6f2170c0ed86ac5cc
Egor Artemyev <artemyev93@protonmail.com>
2022-08-11 23:46:55 +0300

    simplified auth urls

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,28 +1,8 @@
-from django.urls import path, reverse_lazy
-from django.contrib.auth.views import (LoginView, LogoutView,
-                                       PasswordChangeView, PasswordChangeDoneView,
-                                       PasswordResetView, PasswordResetDoneView,
-                                       PasswordResetConfirmView, PasswordResetCompleteView)
+from django.urls import path, include
 from .views import dashboard
 
-app_name = 'account'
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
-    path('password_change/',
-         PasswordChangeView.as_view(success_url=reverse_lazy('account:password_change_done')),
-         name='password_change'),
-    path('password_change/done/', PasswordChangeDoneView.as_view(),
-         name='password_change_done'),
-    path('password_reset/',
-         PasswordResetView.as_view(success_url=reverse_lazy('account:password_reset_done')),
-         name='password_reset'),
-    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-         PasswordResetConfirmView.as_view(success_url=reverse_lazy('account:password_reset_complete')),
-         name='password_reset_confirm'),
-    path('reset/done/',
-         PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
 ]
